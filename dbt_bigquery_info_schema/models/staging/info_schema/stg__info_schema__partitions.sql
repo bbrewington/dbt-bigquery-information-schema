a22284fa9674c4c1d53b7a3617759e9c118fafcc
bbrewington <brent.brewington@gmail.com>
2022-12-11 18:48:16 -0500

    My 'Aha Moment' About Turning Off Models

--- dbt_bigquery_info_schema/models/staging/info_schema/stg__info_schema__partitions.sql
+++ dbt_bigquery_info_schema/models/staging/info_schema/stg__info_schema__partitions.sql
@@ -1,3 +1,8 @@
+--turning this model off b/c drain on GBQ metadata limits (you can change enabled to true if you want it)
+{{ config(
+  enabled=false
+) }}
+
 -- depends_on: {{ ref('stg__info_schema__datasets') }}
 
 {{ get_partitions_over_datasets(exclude_list=['gbq_info_schema']) }}
